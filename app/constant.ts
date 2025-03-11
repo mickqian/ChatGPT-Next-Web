5244657cd0d572502eea5ef8115df04016f373fd
Mick <mickjagger19@icloud.com>
2025-03-11 10:54:16 +0800

    remote claude-3-5

--- app/constant.ts
+++ app/constant.ts
@@ -557,10 +557,10 @@ const anthropicModels = [
   // "claude-3-opus-latest",
   // "claude-3-haiku-20240307",
   // "claude-3-5-haiku-20241022",
-  "claude-3-5-haiku-latest",
+  // "claude-3-5-haiku-latest",
   // "claude-3-5-sonnet-20240620",
   // "claude-3-5-sonnet-20241022",
-  "claude-3-5-sonnet-latest",
+  // "claude-3-5-sonnet-latest",
   //"claude-3-7-sonnet-20250219",
   "claude-3-7-sonnet-latest",
 ];
